43b2f7fa7b5e6ceea7bee1cfd4f9d271fe4b39a2
Pierrick Charron <pierrick@webstart.fr>
2012-10-26 00:59:56 -0400

    Add the .gitattributes file

--- mqseries.c
+++ mqseries.c
@@ -327,7 +327,7 @@ PHP_MINFO_FUNCTION(mqseries)
 	php_info_print_table_start();
 	php_info_print_table_header(2, "mqseries support", "enabled");
 	php_info_print_table_row(2, "Version", PHP_MQSERIES_VERSION);
-	php_info_print_table_row(2, "Revision", "$Revision$");
+	php_info_print_table_row(2, "Revision", "$Id$");
 	php_info_print_table_end();
 }
 /* }}} */

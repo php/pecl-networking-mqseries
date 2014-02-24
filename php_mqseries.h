3fb735e8689a8edc92a202b0f42408421db77776
Pierrick Charron <pierrick@webstart.fr>
2014-02-24 16:49:45 -0500

    Add the list of function to be implemented as part of MQIC7

--- php_mqseries.h
+++ php_mqseries.h
@@ -2,35 +2,35 @@
 Copyright (c) 2003, JAWA Management Software GmbH http://www.jawa.at
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without 
-modification, are permitted provided that the following conditions 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
 are met:
 
-1. Redistributions of source code must retain the above copyright 
+1. Redistributions of source code must retain the above copyright
    notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright 
-   notice, this list of conditions and the following disclaimer in the 
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
-3. The names of the authors may not be used to endorse or promote products 
+3. The names of the authors may not be used to endorse or promote products
    derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
-OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-This code cannot simply be copied and put under the GNU Public License or 
+This code cannot simply be copied and put under the GNU Public License or
 any other GPL-like (LGPL, GPL2) License.
 
     $Id$
 
-Author: Michael Bretterklieber <mbretter@jawa.at>    
+Author: Michael Bretterklieber <mbretter@jawa.at>
 		philippe Tjon A Hen <tjonahen@zonnet.nl>
 		Pierrick Charron <pierrick@php.net>
 */
@@ -100,6 +100,22 @@ PHP_FUNCTION(mqseries_set);
 PHP_FUNCTION(mqseries_error);
 PHP_FUNCTION(mqseries_strerror);
 PHP_FUNCTION(mqseries_bytes_val);
+
+#ifdef HAVE_MQSERIESLIB_V7
+# PHP_FUNCTION(mqseries_bufmh);
+# PHP_FUNCTION(mqseries_cb);
+# PHP_FUNCTION(mqseries_cb_function);
+# PHP_FUNCTION(mqseries_crtmh);
+# PHP_FUNCTION(mqseries_ctl);
+# PHP_FUNCTION(mqseries_dltmh);
+# PHP_FUNCTION(mqseries_dltmp);
+# PHP_FUNCTION(mqseries_inqmp);
+# PHP_FUNCTION(mqseries_mhbuf);
+# PHP_FUNCTION(mqseries_setmp);
+# PHP_FUNCTION(mqseries_stat);
+# PHP_FUNCTION(mqseries_sub);
+# PHP_FUNCTION(mqseries_subrq);
+#endif /* HAVE_MQSERIESLIB_V7 */
 
 #ifdef ZTS
 #define MQSERIES_G(v) TSRMG(mqseries_globals_id, zend_mqseries_globals *, v)
